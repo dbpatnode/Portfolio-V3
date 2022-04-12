fadc741022271dcc2deffc74579bb484f8ad6a74
Daniel Patnode <dbpatnode@gmail.com>
2022-04-12 11:57:54 -0700

    set up emailer with env

--- src/components/contact/contact.component.jsx
+++ src/components/contact/contact.component.jsx
@@ -1,79 +1,45 @@
-// import React, { Component } from 'react';
-// // import BlogItem from '../blog-item/blog-item.component';
-// import ComingSoon from '../coming-soon/coming-soon.component';
-// import './contact.style.scss';
+// import React, { useRef } from "react";
+import emailjs from "emailjs-com";
 
-// export default class Blogs extends Component {
-//     render() {
-//         return (
-//             <div className="blogs-container">
-//                 <ComingSoon page={"CONTACT"} />
-//             </div>
-//         )
-//     }
-// }
-
-
-// import React, { useRef, useState } from 'react';
-import React from 'react';
-// import emailjs from 'emailjs-com';
-import ComingSoon from '../coming-soon/coming-soon.component';
-
-import './contact.style.scss';
+import "./contact.style.scss";
 
 const Contact = () => {
-    // const form = useRef();
+  //   const form = useRef();
 
+  const sendEmail = (e) => {
+    e.preventDefault();
 
+    emailjs
+      .sendForm(
+        process.env.REACT_APP_SERVICE_ID,
+        process.env.REACT_APP_TEMPLATE_ID,
+        e.target,
+        process.env.REACT_APP_EMAILJS_PUBLIC_KEY
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+    e.target.reset();
+  };
 
-    // const SERVICE_KEY = process.env.REACT_APP_EMAILJS_SERVICE_KEY
-    // const TEMPLATE_KEY = process.env.REACT_APP_EMAILJS_TEMPLATE_KEY
-    // const USER_KEY = process.env.REACT_APP_EMAILJS_USER_KEY
-    // const sendEmail = (e) => {
-    //     e.preventDefault();
-
-    //     emailjs.sendForm(SERVICE_KEY, TEMPLATE_KEY, form.current, USER_KEY)
-    //         .then((result) => {
-    //             console.log(result.text);
-    //         }, (error) => {
-    //             console.log(error.text);
-    //         });
-    // };
-
-
-    // // const [firstname, setFirstname] = useState('');
-    // const [name, setName] = useState('');
-    // const [email, setEmail] = useState('');
-
-    // function SubmitButton() {
-    //     if (name && email) {
-    //         return <input type="submit" value="Send" />
-    //     } else {
-    //         return <input type="submit" value="Send" disabled />
-    //     };
-    // };
-
-    return (
-        <div className="contact">
-            <ComingSoon page={"CONTACT"} />
-        </div>
-        /* <h2>Have a question or just want to reach out? Let's get in touch!</h2>
-        <form classref={form} onSubmit={sendEmail} className="email-form">
-
-            <label className="email-form-label">Your Name: </label>
-            <input type="text" name="from_name" value={name} onChange={e => setName(e.target.value)} required />
-            <label className="email-form-label">Email: </label>
-            <input type="email" name="senders_email_address" value={email} onChange={e => setEmail(e.target.value)} required />
-
-
-            <label className="email-form-label">Talk to me: </label>
-            <textarea className="email-message-box" name="message" />
-            <SubmitButton />
-
-        </form > */
-        // </div >
-
-    );
+  return (
+    <form onSubmit={sendEmail}>
+      <label>Name</label>
+      <input type="text" name="name" />
+      <label>Email</label>
+      <input type="email" name="email" />
+      <label>Subject</label>
+      <input type="text" name="subject" />
+      <label>Message</label>
+      <textarea name="message" />
+      <input type="submit" value="Send" />
+    </form>
+  );
 };
 
 export default Contact;
