81bcde101261a7260a33c9604495308ea76f6ab2
Daniel Patnode <dbpatnode@gmail.com>
2022-04-14 10:40:45 -0700

    restyled about me section

--- src/components/about/about.component.jsx
+++ src/components/about/about.component.jsx
@@ -1,26 +1,28 @@
 import React, { Component } from 'react';
 import resumeData from '../../resumeData';
 // import Headshot from './headshot.png'
-import './about.style.scss'
+import './about.style.scss';
 
 export default class About extends Component {
-    render() {
-
-        return (
-            <div id="about">
-                <div className="content-container">
-                    <div className="headshot-container">
-                        <div className="profile-pic"></div>
-                    </div>
-
-                    <div className="information-container">
-                        <h2>About Me</h2>
-                        <p>
-                            {resumeData.aboutme} Feel free to reach out with any inquiries <a href="/contact">here</a>.
-                        </p>
-                    </div>
-                </div>
+  render() {
+    return (
+      <div id='about'>
+        <div className='container'>
+          <div className='headshot-container'>
+            <div className='profile-pic'>
+              <img src='/images/headshot2.png' alt='daniel patnode'></img>
             </div>
-        );
-    }
+          </div>
+
+          <div className='information-container'>
+            <h2>About Me</h2>
+            <p>
+              {resumeData.aboutme} Feel free to reach out with any inquiries{' '}
+              <a href='/contact'>here</a>.
+            </p>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
