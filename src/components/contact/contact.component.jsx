86e7143f2898acc53581981d52b7638f348c1606
Daniel Patnode <dbpatnode@gmail.com>
2022-04-14 12:45:55 -0700

    updated contact form

--- src/components/contact/contact.component.jsx
+++ src/components/contact/contact.component.jsx
@@ -1,6 +1,6 @@
-import React, { useState } from "react";
-import "./contact.style.scss";
-import emailjs from "emailjs-com";
+import React, { useState } from 'react';
+import './contact.style.scss';
+import emailjs from 'emailjs-com';
 
 const Contact = () => {
   const [isSubmitted, setIsSubmitted] = useState(false);
@@ -18,13 +18,13 @@ const Contact = () => {
         process.env.REACT_APP_SERVICE_ID,
         process.env.REACT_APP_TEMPLATE_ID,
         e.target,
-        process.env.REACT_APP_EMAILJS_PUBLIC_KEY
+        process.env.REACT_APP_EMAILJS_PUBLIC_KEY,
       )
       .then(
         (result) => {
           console.log(result.text);
           console.log(result);
-          if (result.text === "OK") {
+          if (result.text === 'OK') {
             setIsSubmitted(!isSubmitted);
           }
         },
@@ -33,13 +33,13 @@ const Contact = () => {
           if (error.text) {
             setError(!error);
           }
-        }
+        },
       );
     e.target.reset();
   };
 
   return (
-    <div className="contact">
+    <div className='contact'>
       <p>
         Have a question? Think I might be a good fit? Just want to chat? Feel
         free to reach out below, I would love to hear from you!
@@ -47,9 +47,9 @@ const Contact = () => {
       <form onSubmit={sendEmail}>
         <div>
           <input
-            type="text"
-            name="name"
-            placeholder="Name..."
+            type='text'
+            name='name'
+            placeholder='Name...'
             onClick={handleClick}
             required
           />
@@ -57,9 +57,9 @@ const Contact = () => {
 
         <div>
           <input
-            type="email"
-            name="email"
-            placeholder="Email..."
+            type='email'
+            name='email'
+            placeholder='Email...'
             onClick={handleClick}
             required
           />
@@ -67,30 +67,35 @@ const Contact = () => {
 
         <div>
           <input
-            type="text"
-            name="subject"
-            placeholder="Subject..."
+            type='text'
+            name='subject'
+            placeholder='Subject...'
             onClick={handleClick}
             required
           />
         </div>
         <div>
-          <textarea name="message" onClick={handleClick} required />
+          <textarea name='message' onClick={handleClick} required />
         </div>
 
         <div>
-          <input type="submit" value="Send" />
+          {/* <input type="submit" value="Send" /> */}
+          <button type='submit'>
+            {/* Send <i class='fa fa-angle-double-right'></i> */}
+            {/* Send <i class='fa fa-arrow-circle-o-right'></i> */}
+            Send <i class='fa fa-paper-plane'></i>
+          </button>
         </div>
       </form>
       {isSubmitted && (
-        <div className="success">
+        <div className='success'>
           <p>Email sent! I'll get back to you shortly! 😊 </p>
         </div>
       )}
       {error && (
-        <div className="error">
+        <div className='error'>
           <p>
-            {"There seems to have been a problem 😮 . Please try again later."}
+            {'There seems to have been a problem 😮 . Please try again later.'}
           </p>
         </div>
       )}
