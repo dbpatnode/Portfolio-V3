534d012a954b7a2d5b2efbc15bfda06b52744012
Daniel Patnode <dbpatnode@gmail.com>
2022-04-15 12:44:56 -0700

    partially styled cards

--- src/components/blog-card/blog-card.component.jsx
+++ src/components/blog-card/blog-card.component.jsx
@@ -3,17 +3,22 @@ import BlogTags from './blog-tags.component';
 import { Link } from 'react-router-dom';
 
 const BlogItem = ({ item }) => {
+  console.log(item);
+
   const { title, pubDate, thumbnail, id, categories } = item;
 
   return (
     <div key={id} className='blog-card'>
-      <Link to={`/blogs/${id}`}>
-        <img src={thumbnail} alt={title} />
-        <h2>{title}</h2>
-      </Link>
-
-      <BlogTags tags={categories} />
-      <p>{pubDate}</p>
+      <div className='wrapper'>
+        <Link to={`/blogs/${id}`}>
+          <img src={thumbnail} alt={title} />
+          <h2>{title}</h2>
+        </Link>
+      </div>
+      <div className='wrapper'>
+        <BlogTags tags={categories} />
+        <p>{pubDate}</p>
+      </div>
     </div>
   );
 };
