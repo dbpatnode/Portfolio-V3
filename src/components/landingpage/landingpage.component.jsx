9b400d31b1a943e37d45e047f587243461b602b1
Daniel Patnode <dbpatnode@gmail.com>
2022-04-16 17:40:12 -0700

    Landing Page from class component to functional component

--- src/components/landingpage/landingpage.component.jsx
+++ src/components/landingpage/landingpage.component.jsx
@@ -1,15 +1,13 @@
-
-import React, { Component } from 'react';
 import HomePage from '../homepage/homepage.component';
 import About from '../about/about.component';
 
-export default class LandingPage extends Component {
-    render() {
-        return (
-            <>
-                <HomePage />
-                <About />
-            </>
-        )
-    }
-}
+const LandingPage = () => {
+  return (
+    <>
+      <HomePage />
+      <About />
+    </>
+  );
+};
+
+export default LandingPage;
