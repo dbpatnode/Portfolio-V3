658218c6920f9ae384b85c2c427c1db0f6e8d554
Daniel Patnode <dbpatnode@gmail.com>
2022-04-17 10:50:13 -0700

    moved to only hamburger nav for both mobile and web

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -12,64 +12,63 @@ const Header = () => {
   return (
     <nav className='header'>
       {/* LOGO */}
-      <NavLink activeClassName='logo' to='/'>
+      {/* <NavLink activeClassName='logo' to='/'>
         <div className='logo-image' />
-      </NavLink>
+      </NavLink> */}
 
-      <div>
-        {/* WEB BUTTONS */}
-        <div className='web-options'>
+      {/* <div> */}
+      {/* WEB BUTTONS */}
+      {/* <div className='web-options'>
           <div className='links'>
             <NavLink exact to='/projects'>
-              {' '}
-              projects{' '}
+              projects
             </NavLink>
             <NavLink to='/blogs'> blogs </NavLink>
             <NavLink to='/resume'> resume </NavLink>
             <NavLink to='/contact'> contact </NavLink>
           </div>
-        </div>
+        </div> */}
 
-        {/* MOBILE HAMBURGER BUTTON*/}
-        <div class='hamburger-wrapper'>
-          <div className={isActive ? 'navigation active' : 'navigation'}>
-            <div className='ham-btn' onClick={ToggleClass}>
-              <span></span>
-              <span></span>
-              <span></span>
+      {/* MOBILE HAMBURGER BUTTON*/}
+      <div class='hamburger-wrapper'>
+        <div className={isActive ? 'navigation active' : 'navigation'}>
+          <div className='ham-btn' onClick={ToggleClass}>
+            <span></span>
+            <span></span>
+            <span></span>
+          </div>
+
+          {/* HAMBURGER BUTTONS */}
+          <div className='links'>
+            <div className='link' onClick={ToggleClass}>
+              <NavLink activeClassName='nav-active' exact to='/'>
+                Home
+              </NavLink>
             </div>
-
-            {/* HAMBURGER BUTTONS */}
-            <div className='links'>
-              <div className='link' onClick={ToggleClass}>
-                <NavLink activeClassName='nav-active' exact to='/'>
-                  Home
-                </NavLink>
-              </div>
-              <div className='link' onClick={ToggleClass}>
-                <NavLink activeClassName='nav-active' to='/projects'>
-                  Projects
-                </NavLink>
-              </div>
-              <div className='link' onClick={ToggleClass}>
-                <NavLink activeClassName='nav-active' to='/blogs'>
-                  Blogs
-                </NavLink>
-              </div>
-              <div className='link' onClick={ToggleClass}>
-                <NavLink activeClassName='nav-active' to='/resume'>
-                  Resume
-                </NavLink>
-              </div>
-              <div className='link' onClick={ToggleClass}>
-                <NavLink activeClassName='nav-active' to='/contact'>
-                  contact
-                </NavLink>
-              </div>
+            <div className='link' onClick={ToggleClass}>
+              <NavLink activeClassName='nav-active' to='/projects'>
+                Projects
+              </NavLink>
+            </div>
+            <div className='link' onClick={ToggleClass}>
+              <NavLink activeClassName='nav-active' to='/blogs'>
+                Blogs
+              </NavLink>
+            </div>
+            <div className='link' onClick={ToggleClass}>
+              <NavLink activeClassName='nav-active' to='/resume'>
+                Resume
+              </NavLink>
+            </div>
+            <div className='link' onClick={ToggleClass}>
+              <NavLink activeClassName='nav-active' to='/contact'>
+                contact
+              </NavLink>
             </div>
           </div>
         </div>
       </div>
+      {/* </div> */}
     </nav>
   );
 };
