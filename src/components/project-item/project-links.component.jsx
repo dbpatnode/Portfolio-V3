c33033a7bdfaea3628486406c260e68b14250926
Daniel Patnode <dbpatnode@gmail.com>
2022-04-19 12:08:35 -0700

    mobile buttons

--- src/components/project-item/project-links.component.jsx
+++ src/components/project-item/project-links.component.jsx
@@ -1,17 +1,8 @@
-// import { useNavigate } from 'react-router-dom';
-
 const ProjectLinks = ({ githubUrl, linkUrl, youtube }) => {
-  // const navigate = useNavigate();
   return (
     <div className='bottom'>
       <div className='link-container'>
         {githubUrl ? (
-          // <div className='swing link'>
-          //   <a href={githubUrl} target='_blank' rel='noreferrer'>
-          //     <p>GitHub</p> <br />
-          //     <i className='fa fa-github' />
-          //   </a>
-          // </div>
           <button
             className='swing'
             onClick={() => {
@@ -23,13 +14,6 @@ const ProjectLinks = ({ githubUrl, linkUrl, youtube }) => {
           </button>
         ) : null}
         {linkUrl ? (
-          // <div className='swing link'>
-          //   <a href={linkUrl} target='_blank' rel='noreferrer'>
-          //     <p>LiveLink</p> <br />
-          //     <i className='fa fa-link' />
-          //   </a>
-          // </div>
-
           <button
             className='swing'
             onClick={() => {
@@ -41,13 +25,6 @@ const ProjectLinks = ({ githubUrl, linkUrl, youtube }) => {
           </button>
         ) : null}
         {youtube ? (
-          // <div className='swing link'>
-          //   <a href={youtube} target='_blank' rel='noreferrer'>
-          //     <p>Youtube</p> <br />
-          //     <i className='fab fa-youtube' />
-          //   </a>
-          // </div>
-
           <button
             className='swing'
             onClick={() => {
